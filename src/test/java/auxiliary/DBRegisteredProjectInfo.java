f10c85382c6d1eaa136296fba571c159e3a5dc9f
sagi stav <sagistav@mail.sapir.ac.il>
2025-01-01 18:57:51 +0200

    add file

---
+++ src/test/java/auxiliary/DBRegisteredProjectInfo.java
@@ -0,0 +1,9 @@
+package auxiliary;
+import java.util.ArrayList;
+
+public class DBRegisteredProjectInfo {
+	public int projectId;
+    public ArrayList<String> studentList = new ArrayList<String>();
+	public String academicAdviser;
+
+}
---
+++ src/test/java/auxiliary/DBSuggestedProjectInfo.java
@@ -0,0 +1,13 @@
+package auxiliary;
+import java.text.SimpleDateFormat;
+
+public class DBSuggestedProjectInfo {
+    public String firstName;
+    public String lastName;
+	public String phone;
+	public String email;
+    public String organization;
+    public String projectName;
+    public String description;
+    public int numberOfHours;
+}
