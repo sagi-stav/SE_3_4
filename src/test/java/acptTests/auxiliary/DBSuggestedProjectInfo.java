885e21c5a008c2be52214d07ee1fac2c7e4c44c6
sagi stav <sagistav@mail.sapir.ac.il>
2025-01-02 18:58:56 +0200

    add files

---
+++ src/test/java/acptTests/auxiliary/DBSuggestedProjectInfo.java
@@ -0,0 +1,13 @@
+package acptTests.auxiliary;
+import java.text.SimpleDateFormat;
+
+public class DBSuggestedProjectInfo {
+    public String firstName;
+    public String lastName;
+	public String phone;
+	public String email;
+    public String organization;
+    public String projectName;
+    public String description;
+    public int numberOfHours;
+}
